7ae062f50a1020a523244fc4d8b67c0b37dc0821
Dibya12345 <48878439+Dibya12345@users.noreply.github.com>
2020-07-28 21:59:38 +0530

    Update and rename grayscale.py.py to grayscale.py

--- grayscale.py
+++ grayscale.py
@@ -1,13 +1,12 @@
-#First open the image
-#Now convert the colouerd into grayscale by following method =B*0.2989+G*0.5870+R*0.1140
-#repalce the average value in r,g,b in pixel the coloured image
+#importing necessary packages
 from PIL import Image
 import numpy as np
 # opens the image
 image=Image.open('flower.jpeg')
 #converting the image into array
 image_array=np.array(image)
-#Now taking the average of each pixel in the image
+#Now convert the colouerd into grayscale by following method (B*0.2989+G*0.5870+R*0.1140)
+#repalce the average value in r,g,b in pixel the coloured image
 for i in range(image.height):
     for j in range(image.width):
         average=(image_array[i][j][0]*0.2989+image_array[i][j][1]*0.5870+image_array[i][j][2]*0.1140)
@@ -15,4 +14,5 @@ for i in range(image.height):
         image_array[i][j][1]=average
         image_array[i][j][2]=average
 new_img=Image.fromarray(image_array,'RGB')
+#dislay the image
 new_img.show()                                          
